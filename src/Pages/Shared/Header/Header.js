7f26514e8ee71994c639378b5697be4819a1f3f0
AkashAbrrar <mabrrar9@gmail.com>
2022-04-12 00:40:02 +0600

    all services data and responsiveness are added

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h2>I am the fancy header for this page</h2>
+        </header>
+    );
+};
+
+export default Header;
