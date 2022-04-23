baf501d27475373e3be9cc82b6766d8af072c094
AkashAbrrar <mabrrar9@gmail.com>
2022-04-23 15:53:38 +0600

    this commit for database and management system added and remove system for server site

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import PageTitle from '../Shared/PageTItle/PageTitle';
 
 const About = () => {
     return (
         <div>
+            <PageTitle title="About"></PageTitle>
             <h2>Do you wanna About Us</h2>
         </div>
     );
